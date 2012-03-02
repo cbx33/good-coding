743ec3b09acbf76fcd8ff4edd3d00a19494101e1
Peter Savage <silentkeystroke@googlemail.com>
2012-03-02 20:25:23 +0000

    Now detects snippets that are not complete, still only deals with python

--- converter.py
+++ converter.py
@@ -2,6 +2,7 @@ from pygments import highlight
 from pygments.lexers import PythonLexer
 from pygments.formatters import HtmlFormatter
 import re
+import os
 
 HEADER = '<link rel="stylesheet" href="style.css">'
 
@@ -9,8 +10,14 @@ class tip():
 	def __init__(self, name):
 		self.name = name
 		self.path = self.name + "/"
+		self.snippet_path = self.path + "snippets/"
 		self.refs = {}
 		self.tip_data = ""
+		self.langs = []
+		self.snippet_file_list = self.cache_snippet_dir()
+		
+	def cache_snippet_dir(self):
+		 return os.listdir(self.snippet_path)
 
 	def get_tip_data(self):
 		f = open(self.path + "tip.html")
@@ -24,8 +31,34 @@ class tip():
 		f.write(output)
 		f.close()
 
+	def check_snippet(self, name):
+		lang_first_time = False
+		if self.langs == []:
+			lang_first_time = True
+			check_lang = []
+		else:
+			check_lang = self.langs[:]
+		for filename in self.snippet_file_list:
+			if name in filename:
+				path_split = os.path.splitext(filename)
+				extn = path_split[1].split(".")[1]
+				if lang_first_time == True:
+					self.langs.append(extn)
+				else:
+					check_lang.remove(extn)
+		if len(check_lang) == 0:
+			return True
+		else:
+			return check_lang
+		print self.langs
 
 	def process_snippet(self, snippet_data):
+		check_snippet = self.check_snippet(snippet_data[1])
+		if check_snippet != True:
+			print (", ").join(check_snippet) + " is missing from snippet: " + snippet_data[1]
+			
+		exit(0)
+			
 		f = open(self.path + "snippets/" + snippet_data[1] + ".py")
 		snippet_code = f.read()
 		snippet_code_lines = snippet_code.split("\n")
@@ -39,6 +72,8 @@ class tip():
 		self.tip_data = self.tip_data.replace(snippet_data[0], highlight(snippet_code, PythonLexer(), HtmlFormatter(linenos=True,)))
 
 	def process_tip(self):
+		self.get_tip_data()
+		
 		#Process snippets within the tip
 		snippets = re.findall("(<tip id=\"(snip\d*)\" />)", self.tip_data)
 		for snippet in snippets:
@@ -51,7 +86,6 @@ class tip():
 
 
 new_tip = tip("tip1")
-new_tip.get_tip_data()
 new_tip.process_tip()
 
 #print highlight(f.read(), PythonLexer(), HtmlFormatter(linenos=True,))
