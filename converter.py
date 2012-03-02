2d3bf99d25319fbd02b7f857da89955c7a2abcf7
Peter Savage <silentkeystroke@googlemail.com>
2012-03-02 18:11:37 +0000

    Classified and functionalised - more work to do

--- converter.py
+++ converter.py
@@ -3,42 +3,51 @@ from pygments.lexers import PythonLexer
 from pygments.formatters import HtmlFormatter
 import re
 
-TIP_NAME = "tip1"
-path = TIP_NAME + "/"
-
 HEADER = '<link rel="stylesheet" href="style.css">'
-REFS = {}
 
-f = open(path + "tip.html")
-tip_html = f.read()
-f.close()
+class tip():
+	def __init__(self, name):
+		self.name = name
+		self.path = self.name + "/"
+		self.refs = {}
+		self.tip_data = ""
 
-snippets = re.findall("(<tip id=\"snip(\d*)\" />)", tip_html)
+	def get_tip_data(self):
+		f = open(self.path + "tip.html")
+		self.tip_data = f.read()
+		f.close()		
 
-for snippet in snippets:
-	f = open(path + "snippets/" + "snip" + snippet[1] + ".py")
-	snippet_code = f.read()
-	snippet_code_lines = snippet_code.split("\n")
-	for line in snippet_code_lines:
-		references = re.search("(<<<ref#(\d)>>>)", line)
-		if references:
-			ref = references.groups()[1]
-			REFS[ref] = snippet_code_lines.index(line) + 1
-			snippet_code = snippet_code.replace(references.groups()[0], "")
-	f.close()
-	tip_html = tip_html.replace(snippet[0], highlight(snippet_code, PythonLexer(), HtmlFormatter(linenos=True,)))
+	def process_tip(self):
+		snippets = re.findall("(<tip id=\"snip(\d*)\" />)", self.tip_data)
 
-snippet_refs = re.findall("(<<<ref#(\d)>>>)", tip_html)
+		for snippet in snippets:
+			f = open(self.path + "snippets/" + "snip" + snippet[1] + ".py")
+			snippet_code = f.read()
+			snippet_code_lines = snippet_code.split("\n")
+			for line in snippet_code_lines:
+				references = re.search("(<<<ref#(\d)>>>)", line)
+				if references:
+					ref = references.groups()[1]
+					self.refs[ref] = snippet_code_lines.index(line) + 1
+					snippet_code = snippet_code.replace(references.groups()[0], "")
+			f.close()
+			self.tip_data = self.tip_data.replace(snippet[0], highlight(snippet_code, PythonLexer(), HtmlFormatter(linenos=True,)))
 
-for snippet_ref in snippet_refs:
-	#print REFS[str(snippet_ref[1])]
-	tip_html = tip_html.replace(snippet_ref[0], str(REFS[str(snippet_ref[1])]))
-	
-output = HEADER + tip_html
+		snippet_refs = re.findall("(<<<ref#(\d)>>>)", self.tip_data)
 
-f = open("tmp.html", "w")
-f.write(output)
-f.close()
+		for snippet_ref in snippet_refs:
+			#print REFS[str(snippet_ref[1])]
+			self.tip_data = self.tip_data.replace(snippet_ref[0], str(self.refs[str(snippet_ref[1])]))
+			
+		output = HEADER + self.tip_data
+
+		f = open("tmp.html", "w")
+		f.write(output)
+		f.close()
+
+new_tip = tip("tip1")
+new_tip.get_tip_data()
+new_tip.process_tip()
 
 #print highlight(f.read(), PythonLexer(), HtmlFormatter(linenos=True,))
 #print HtmlFormatter().get_style_defs('.highlight')
