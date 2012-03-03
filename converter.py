29020dc74aa3f73c4b3094dde7c3b360a8ee5e94
Peter Savage <silentkeystroke@googlemail.com>
2012-03-03 18:42:34 +0000

    * Now handles multiple snippet formats, but outputs as single block, containing multiple code printouts. * Will exit with non-zero status if not all the snippet files are present. Checks against first snippet.

--- converter.py
+++ converter.py
@@ -1,5 +1,6 @@
 from pygments import highlight
 from pygments.lexers import PythonLexer
+from pygments.lexers import get_lexer_for_filename
 from pygments.formatters import HtmlFormatter
 import re
 import os
@@ -50,16 +51,11 @@ class tip():
 			return True
 		else:
 			return check_lang
-		print self.langs
 
-	def process_snippet(self, snippet_data):
-		check_snippet = self.check_snippet(snippet_data[1])
-		if check_snippet != True:
-			print (", ").join(check_snippet) + " is missing from snippet: " + snippet_data[1]
-			
-		exit(0)
-			
-		f = open(self.path + "snippets/" + snippet_data[1] + ".py")
+	def process_snippet_lang(self, snippet_name, lang):
+		snippet_filename = snippet_name + "." + lang
+		snippet_lexer = get_lexer_for_filename(snippet_filename)
+		f = open(self.snippet_path+ "/" + snippet_filename)
 		snippet_code = f.read()
 		snippet_code_lines = snippet_code.split("\n")
 		for line in snippet_code_lines:
@@ -69,7 +65,23 @@ class tip():
 				self.refs[ref] = snippet_code_lines.index(line) + 1
 				snippet_code = snippet_code.replace(references.groups()[0], "")
 		f.close()
-		self.tip_data = self.tip_data.replace(snippet_data[0], highlight(snippet_code, PythonLexer(), HtmlFormatter(linenos=True,)))
+		return highlight(snippet_code, snippet_lexer, HtmlFormatter(linenos=True,))
+
+
+	def process_snippet(self, snippet_data):
+		snippet_name = snippet_data[1]
+		snippet_ref = snippet_data[0]
+		check_snippet = self.check_snippet(snippet_name)
+		if check_snippet != True:
+			print (", ").join(check_snippet) + " is missing from snippet: " + snippet_name
+			exit(1)
+
+		snippet_blocks = ""
+
+		for lang in self.langs:
+			snippet_blocks += self.process_snippet_lang(snippet_name, lang)
+
+		self.tip_data = self.tip_data.replace(snippet_ref, snippet_blocks)
 
 	def process_tip(self):
 		self.get_tip_data()
@@ -87,6 +99,6 @@ class tip():
 
 new_tip = tip("tip1")
 new_tip.process_tip()
-
+new_tip.output_tip()
 #print highlight(f.read(), PythonLexer(), HtmlFormatter(linenos=True,))
 #print HtmlFormatter().get_style_defs('.highlight')
